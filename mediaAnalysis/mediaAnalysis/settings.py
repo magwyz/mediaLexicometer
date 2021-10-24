94660663ba4f20aff466415c3ec2247fd4abb418
Adrien Maglo <adrien@videolabs.io>
2021-10-24 10:39:29 +0200

    settings: change the locale

--- mediaAnalysis/mediaAnalysis/settings.py
+++ mediaAnalysis/mediaAnalysis/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'Europe/Paris'
 
