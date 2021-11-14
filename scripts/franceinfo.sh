9029cb781eb7e2fa357704515d5f809f9ed032d5
Adrien Maglo <adrien@videolabs.io>
2021-11-14 21:58:13 +0100

    Move the example channel scripts

--- scripts/franceinfo.sh
+++ scripts/franceinfo.sh
@@ -2,4 +2,3 @@
 
 tmux new -d -s dvb1 dvbv5-zap --adapter 1 -r -p --all-pids -c channels_v5.conf "franceinfo (GR1 A)"
 tmux new -d -s process1 \; send-keys "source ../venv/bin/activate" Enter "python liveSpeechToText.py --adapter-drv /dev/dvb/adapter1/dvr0 franceinfo" Enter
-
