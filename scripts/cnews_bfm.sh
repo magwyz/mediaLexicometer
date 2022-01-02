20990c503ac93a63b4ef9d6e0dc9c2712e67329b
Adrien Maglo <adrien@videolabs.io>
2022-01-02 22:21:53 +0100

    scripts: update the name of the tmux sessions

--- scripts/cnews_bfm.sh
+++ scripts/cnews_bfm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-tmux new -d -s dvb0 dvbv5-zap --adapter 0 -r -p --all-pids -c channels_v5.conf "cnews(ntn)"
-tmux new -d -s process0 \; send-keys "source ../venv/bin/activate" Enter "python liveSpeechToText.py --adapter-drv /dev/dvb/adapter0/dvr0 cnews bfmtv" Enter
+tmux new -d -s dvb_cnews_bfmtv dvbv5-zap --adapter 0 -r -p --all-pids -c ~/channels_v5.conf "cnews(ntn)"
+tmux new -d -s process_cnews_bfmtv \; send-keys "source ../venv/bin/activate" Enter "python liveSpeechToText.py --adapter-drv /dev/dvb/adapter0/dvr0 cnews bfmtv" Enter
 
--- scripts/lci.sh
+++ scripts/lci.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-tmux new -d -s dvb2 dvbv5-zap --adapter 2 -r -p --all-pids -c channels_v5.conf "LCI(CNH)"
-tmux new -d -s process2 \; send-keys "source ../venv/bin/activate" Enter "python liveSpeechToText.py --adapter-drv /dev/dvb/adapter2/dvr0 lci" Enter
+tmux new -d -s dvb_lci dvbv5-zap --adapter 2 -r -p --all-pids -c ~/channels_v5.conf "LCI(CNH)"
+tmux new -d -s process_lci \; send-keys "source ../venv/bin/activate" Enter "python liveSpeechToText.py --adapter-drv /dev/dvb/adapter2/dvr0 lci" Enter
 
